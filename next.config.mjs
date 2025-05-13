98879a5ded5aa80a7a96b5550c07e763dd5f5a39
1965Aru <arundhatirothe03@gmail.com>
2025-05-14 00:21:11 +0530

    changes made

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,12 @@ const nextConfig = {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`
-      }
-    ]
-  }
+  return [
+    {
+      source: "/api/:path*",
+      destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`
+    }
+  ];
+}
 };
 export default nextConfig;
